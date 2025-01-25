e0a36a48ecb36db81e9b45ea3b781da79a07fb11
Jonathan Cunningham <jonathanscunningham@gmail.com>
2025-01-25 14:05:10 -0600

    .

---
+++ .config/nvim/lua/configs/remap.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>=", vim.cmd.Ex)
+
+vim.keymap.set("n", "<c-k>", "<cmd>wincmd k<cr>")
+vim.keymap.set("n", "<c-j>", "<cmd>wincmd j<cr>")
+vim.keymap.set("n", "<c-l>", "<cmd>wincmd l<cr>")
+vim.keymap.set("n", "<c-h>", "<cmd>wincmd h<cr>")
+
+vim.keymap.set("n", "<leader>bb", "<cmd>e#<cr>")
+
+vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.set("n", "<C-S-Right>", "<cmd>:vertical resize -1<cr>")
+vim.keymap.set("n", "<C-S-Left>", "<cmd>:vertical resize +1<cr>")
+vim.keymap.set("n", "<leader>s", "<cmd>SymbolsOutline<cr>")
